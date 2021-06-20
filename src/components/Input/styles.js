2c31e25b12f57f8e842fc9d631d4f62810562498
Henrique Luís Oliveira Marques <henriquestudo@outlook.com>
2021-06-20 00:48:05 -0300

    Input Component

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,4 +1,17 @@
 //styled-components
 import styled from "styled-components/native";
 
-export const CustomInput = styled.TextInput``;
+export const CustomInput = styled.TextInput.attrs({
+  placeholderTextColor: "#ffffff60",
+})`
+  width: 90%;
+
+  margin-bottom: 15px;
+  padding: 10px;
+
+  font-size: 17px;
+  color: #ffffff;
+
+  background: #00000060;
+  border-radius: 10px;
+`;
